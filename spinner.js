63007cd90f405c7ef775de849f6bd67c29ad71cf
ebussiere <eddy@embtech.ca>
2020-09-23 01:58:29 +0000

    deleted spinner

--- spinner.js
+++
@@ -1,7 +0,0 @@
-const charArray = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
-const stepDuration = 200;
-for (let i = 0; i < charArray.length; i++) {
-  setTimeout(() => {
-    process.stdout.write('\r' + charArray[i] + "    ");
-  }, i * stepDuration);
-}
